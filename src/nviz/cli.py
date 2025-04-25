aa6be57395135c4af4bf0069c71e3b4585063a74
d33bs <ekgto445@gmail.com>
2025-04-25 11:15:38 -0600

    add nviz cli

---
+++ src/nviz/cli.py
@@ -0,0 +1,162 @@
+"""
+CLI for onesentence
+"""
+
+import fire
+import sys
+from typing import Optional, Union, Dict, Tuple, List
+from nviz.image import tiff_to_zarr, tiff_to_ometiff
+from nviz.view import view_zarr_with_napari, view_ometiff_with_napari
+
+
+class nVizCLI:
+    def tiff_to_zarr(
+        self,
+        image_dir: str,
+        output_path: str,
+        channel_map: Dict[str, str],
+        scaling_values: Union[List[int], Tuple[int]],
+        label_dir: Optional[str] = None,
+        ignore: Optional[List[str]] = ["Merge"],
+    ) -> str:
+        """
+        CLI interface for converting TIFF images to Zarr format.
+
+        Args:
+            image_dir (str):
+                Directory containing TIFF image files.
+            output_path (str):
+                Path to save the output OME-Zarr file.
+            channel_map (Dict[str, str]):
+                Mapping from filename codes to channel names.
+            scaling_values (Union[List[int], Tuple[int]]):
+                Scaling values for the images.
+            label_dir (Optional[str]):
+                Directory containing label TIFF files. Defaults to None.
+            ignore (Optional[List[str]]):
+                List of filename codes to ignore.
+                Defaults to ["Merge"], which is a
+                code for merged images.
+
+        Returns:
+            str: Path to the output OME-Zarr file.
+        """
+
+        return tiff_to_zarr(
+            image_dir=image_dir,
+            output_path=output_path,
+            channel_map=channel_map,
+            scaling_values=scaling_values,
+            label_dir=label_dir,
+            ignore=ignore,
+        )
+
+    def tiff_to_ometiff(
+        self,
+        image_dir: str,
+        output_path: str,
+        channel_map: Dict[str, str],
+        scaling_values: Union[List[int], Tuple[int]],
+        label_dir: Optional[str] = None,
+        ignore: Optional[List[str]] = ["Merge"],
+    ) -> str:
+        """
+        CLI interface for converting TIFF images to OME-TIFF format.
+
+        Args:
+            image_dir (str):
+                Directory containing TIFF image files.
+            output_path (str):
+                Path to save the output OME-Zarr file.
+            channel_map (Dict[str, str]):
+                Mapping from filename codes to channel names.
+            scaling_values (Union[List[int], Tuple[int]]):
+                Scaling values for the images.
+            label_dir (Optional[str]):
+                Directory containing label TIFF files. Defaults to None.
+            ignore (Optional[List[str]]):
+                List of filename codes to ignore.
+                Defaults to ["Merge"], which is a
+                code for merged images.
+
+        Returns:
+            str: Path to the output OME-TIFF file.
+        """
+
+        return tiff_to_ometiff(
+            image_dir=image_dir,
+            output_path=output_path,
+            channel_map=channel_map,
+            scaling_values=scaling_values,
+            label_dir=label_dir,
+            ignore=ignore,
+        )
+
+    def view_zarr(
+        self,
+        zarr_dir: str,
+        scaling_values: Union[List[int], Tuple[int]],
+        headless: Optional[bool] = False,
+    ) -> Optional[str]:
+        """
+        CLI interface for viewing Zarr files with napari.
+
+        Args:
+            zarr_dir (str):
+                Path to the Zarr file.
+            scaling_values (Union[List[int], Tuple[int]]):
+                Scaling values for the image.
+            headless (Optional[bool]):
+                Whether to run in headless mode
+                (where we don't run napari and hand
+                back the viewer object instead).
+                Defaults to False.
+
+        Returns:
+            Optional[str]:
+                The napari viewer object if not headless,
+                otherwise None.
+        """
+
+        return view_zarr_with_napari(
+            zarr_dir=zarr_dir, scaling_values=scaling_values, 
+            headless=headless
+        )
+    
+    def view_ometiff(
+        self,
+        ometiff_path: str,
+        scaling_values: Union[List[int], Tuple[int]],
+        headless: Optional[bool] = False,
+    ) -> Optional[str]:
+        """
+        CLI interface for viewing OME-TIFF files with napari.
+
+        Args:
+            ometiff_path (str):
+                Path to the OME-TIFF file.
+            scaling_values (Union[List[int], Tuple[int]]):
+                Scaling values for the image.
+            headless (Optional[bool]):
+                Whether to run in headless mode
+                (where we don't run napari and hand
+                back the viewer object instead).
+                Defaults to False.
+
+        Returns:
+            Optional[str]:
+                The napari viewer object if not headless,
+                otherwise None.
+        """
+
+        return view_ometiff_with_napari(
+            ometiff_path=ometiff_path, scaling_values=scaling_values, 
+            headless=headless
+        )
+
+
+def trigger():
+    """
+    Trigger the CLI to run.
+    """
+    fire.Fire(nVizCLI)
---
+++ tests/test_cli.py
@@ -0,0 +1,115 @@
+"""
+Tests CLI capabilities for nviz
+"""
+
+import pytest
+from tests.utils import run_cli_command
+
+
+def test_cli_util():
+    """
+    Test the run_cli_command for successful output
+    """
+
+    _, _, returncode = run_cli_command(["echo", "'hello world'"])
+
+    assert returncode == 0
+
+    assert returncode == 0
+
+
+@pytest.mark.parametrize(
+    "image_dir, output_path, channel_map, scaling_values, label_dir, ignore",
+    [
+        (
+            "tests/data/random_tiff_z_stacks/Z99",
+            "output.zarr",
+            (
+                '{ "111": "Channel A", "222": "Channel B", "333": "Channel C",'
+                ' "444": "Channel D", "555": "Channel E", }'
+            ),
+            "(1.0, 0.1, 0.1)",
+            "None",
+            "None",
+        )
+    ],
+)
+def test_cli_tiff_to_zarr(
+    tmp_path, image_dir, output_path, channel_map, scaling_values, label_dir, ignore
+):
+    """
+    Test CLI use of tiff to zarr and viewing capabilities
+    """
+    stdout, _, returncode = run_cli_command(
+        [
+            "nviz",
+            "tiff_to_zarr",
+            image_dir,
+            (output_path := f"{tmp_path}/{output_path}"),
+            channel_map,
+            scaling_values,
+            label_dir,
+            ignore,
+        ]
+    )
+
+    assert returncode == 0
+    assert stdout.strip() == output_path
+
+    stdout, stderr, returncode = run_cli_command(
+        ["nviz", "view_zarr", output_path, scaling_values, "True"]
+    )
+
+    # napari has difficulties when used headlessly through the cli
+    # so we check that the output shows we returned a viewer object.
+    # This isn't a typical usecase because we can't use the viewer
+    # object through a CLI interface (we only enable it for testing).
+    assert stdout.strip() == "napari.Viewer: napari"
+
+@pytest.mark.parametrize(
+    "image_dir, output_path, channel_map, scaling_values, label_dir, ignore",
+    [
+        (
+            "tests/data/random_tiff_z_stacks/Z99",
+            "output.zarr",
+            (
+                '{ "111": "Channel A", "222": "Channel B", "333": "Channel C",'
+                ' "444": "Channel D", "555": "Channel E", }'
+            ),
+            "(1.0, 0.1, 0.1)",
+            "None",
+            "None",
+        )
+    ],
+)
+def test_cli_tiff_to_ometiff(
+    tmp_path, image_dir, output_path, channel_map, scaling_values, label_dir, ignore
+):
+    """
+    Test CLI use of tiff to ome-tiff and viewing capabilities
+    """
+    stdout, _, returncode = run_cli_command(
+        [
+            "nviz",
+            "tiff_to_ometiff",
+            image_dir,
+            (output_path := f"{tmp_path}/{output_path}"),
+            channel_map,
+            scaling_values,
+            label_dir,
+            ignore,
+        ]
+    )
+
+    assert returncode == 0
+    assert stdout.strip() == output_path
+
+    stdout, _, returncode = run_cli_command(
+        ["nviz", "view_ometiff", output_path, scaling_values, "True"]
+    )
+
+    # napari has difficulties when used headlessly through the cli
+    # so we check that the output shows we returned a viewer object.
+    # This isn't a typical usecase because we can't use the viewer
+    # object through a CLI interface (we only enable it for testing).
+    assert stdout.strip() == "napari.Viewer: napari"
